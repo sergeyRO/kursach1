6aec2767dedfcb3a10e0c136644584e3ceef9152
SergeyRO <sergey_r.o@mail.ru>
2022-06-07 22:54:55 +0300

    Готовая работа

--- main.py
+++ main.py
@@ -1,86 +1,82 @@
-import json
+import datetime
 import requests
-from pprint import pprint
 import urllib.request
-from PIL import Image
+import json
+import os.path
+from datetime import datetime
+from tqdm import tqdm
 
-# Нужно написать программу для резервного копирования фотографий
-# с профиля(аватарок) пользователя vk в облачное хранилище Яндекс.Диск.
-#
-# Для названий фотографий использовать количество лайков, если количество
-# лайков одинаково, то добавить дату загрузки.
-
-#Информацию по сохраненным фотографиям сохранить в json-файл.
-
-# Нужно написать программу, которая будет:
-#
-# 1 Получать фотографии с профиля. Для этого нужно использовать метод photos.get.
-# 2 Сохранять фотографии максимального размера(ширина/высота в пикселях) на Я.Диске.
-# 3 Для имени фотографий использовать количество лайков.
-# 4 Сохранять информацию по фотографиям в json-файл с результатами.
-
-# Входные данные:
-# Пользователь вводит:
-#
-# id пользователя vk;
-# токен с Полигона Яндекс.Диска. Важно: Токен публиковать в github не нужно!
-# Выходные данные:
-# json-файл с информацией по файлу:
-#     [{
-#     "file_name": "34.jpg",
-#     "size": "z"
-#     }]
-# Измененный Я.диск, куда добавились фотографии.
-class YaUploader:
-
-    def __init__(self, token):
-        self.token = token
-
-    def upload(self, likes, file_path):
-        url = 'https://cloud-api.yandex.net:443'
-        params = {'path': 'PHOTO_VK/'+likes+'.jpg', 'overwrite': 'true'}
-        headers = {"Authorization": self.token}
-        url_file = requests.get(url+'/v1/disk/resources/upload', headers=headers, params=params).json()['href']
-        print(url_file)
-        urllib.request.urlretrieve(
-            file_path,
-            likes+'.jpg')
-        #img = Image.open("gfg.png")   open(file_path, 'rb')
-        response = requests.put(url_file, files={'file':file_path})
-        response.raise_for_status()
-        if response.status_code == 201:
-            return f'Файл загружен на яндекс диск'
-
-
-
-def photoVk_to_yaDisk(TOKEN_VK, YANDEX):
+def photoVk_to_Disk(TOKEN_VK, count_photo=5):
     version_api_vk = 5.131
     url = 'https://api.vk.com/method/'
     method = 'photos.get'
     profile_photo_vk = requests.get(
         f'{url}{method}?user_ids=begemot_korovin&'
-        f'photo_sizes=1&extended=1&'
+        f'photo_sizes=1&extended=1&count={count_photo}&'
         f'album_id=profile&access_token={TOKEN_VK}'
         f'&v={version_api_vk}').json()['response']
-
-    for item in profile_photo_vk['items']:
+    list_files = []
+    print("Загрузка файлов из VK на локальный диск:   ")
+    for item in tqdm(profile_photo_vk['items']):
         for key, value in item.items():
             if key == 'likes':
                 likes = value['count']
             if key == 'sizes':
-                height = 0
+                height_ = 0
                 for photo in value:
-                    if photo['height'] > height:
-                        height = photo['height']
+                    if photo['height'] > height_:
+                        height_ = photo['height']
                         url_photo = photo['url']
-        #pprint(f'{likes}    {url_photo}')
-        YANDEX.upload(str(likes), url_photo)
+                        size_ = photo['type']
+        file = str(likes) + '.jpg'
+        if os.path.exists(file):
+            file = str(likes) + '_' + str(datetime.date(datetime.now())) + '.jpg'
+        urllib.request.urlretrieve(url_photo, file)
+        dict = {}
+        dict['file_name'] = file
+        dict['size'] = size_
+        list_files.append(dict)
+    return list_files
 
-    return profile_photo_vk
+def photoDisk_to_YADisk(token, array):
+    list_files_YA = []
+    url = 'https://cloud-api.yandex.net:443'
+    params = {'path': 'PHOTO_VK'}
+    headers = {"Authorization": token}
+    requests.put(url + '/v1/disk/resources', headers=headers, params=params)
+    print("Загрузка файлов с диска на Яндекс диск и создание лога:   ")
+
+    for item in tqdm(array):
+        for key, val in item.items():
+            if key == 'file_name':
+                file_name = val
+            if key == 'size':
+                size_ = val
+
+        params = {'path': 'PHOTO_VK/' + file_name, 'overwrite': 'true'}
+        headers = {"Authorization": token}
+        url_file = requests.get(url + '/v1/disk/resources/upload', headers=headers, params=params).json()['href']
+        response = requests.put(url_file, data=open(file_name, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            dict = {}
+            dict['file_name'] = file_name
+            dict['size'] = size_
+            list_files_YA.append(dict)
+    with open('info.json', 'w') as f:
+        json.dump(list_files_YA, f)
+    return True
+
+def drop_files(list_files):
+    for item in list_files:
+        if (os.path.isfile(item['file_name'])):
+            os.remove(item['file_name'])
+    print("Загрузка завершена!!!")
 
 if __name__ == '__main__':
     TOKEN_VK = 'a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd'
-    TOKEN_YA_DISK = 'AQAAAAAvigESAADLW_SDgmkCJUIuq3-AUBI_m-Y'
+    TOKEN_YA_DISK = ''
 
-    yandex = YaUploader(TOKEN_YA_DISK)
-    pprint(photoVk_to_yaDisk(TOKEN_VK, yandex))
+    list_files_download = photoVk_to_Disk(TOKEN_VK)
+    photoDisk_to_YADisk(TOKEN_YA_DISK, list_files_download)
+    drop_files(list_files_download)
